c928214248f187880151779081f43d7fb502de89
Sergio Santiago <sergio@secture.com>
2025-08-13 04:17:53 +0200

    docs(hammerspoon): add example init.lua with setup instructions

---
+++ hammerspoon/init.lua
@@ -0,0 +1,15 @@
+-- ~/.hammerspoon/init.lua
+
+-- Example init.lua for Hammerspoon
+-- --------------------------------
+-- This file is provided ONLY as a reference for setup.
+-- It will NOT run automatically unless placed (or its contents copied) into:
+--     ~/.hammerspoon/init.lua
+--
+-- If you already have a ~/.hammerspoon/init.lua, simply add the require line below to it.
+-- This tells Hammerspoon to load the Launchpad Shortcut Deck module from:
+--     ~/.hammerspoon/launchpad-shortcut-deck/init.lua
+--
+-- For installation instructions, see the README.md section "Hammerspoon Setup".
+
+require('launchpad-shortcut-deck')
